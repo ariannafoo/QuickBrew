4bfef073c3882e7a7735576c456188c96f98fe0c
Arianna Foo <fooar@sheridancollege.ca>
2025-04-16 14:00:03 -0400

    Initial Commit

---
+++ QuickBrew/QuickBrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickBrewApp.swift
+//  QuickBrew
+//
+//  Created by Arianna Foo on 2025-04-16.
+//
+
+import SwiftUI
+
+@main
+struct QuickBrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
